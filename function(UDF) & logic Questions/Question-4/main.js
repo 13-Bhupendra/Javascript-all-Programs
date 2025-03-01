b0175f10a2865edcb156d7fb0f84499a5ab26494
Bhupendra Patil <bhupendrakp2008@gmail.com>
2025-03-01 18:24:08 +0530

    Add files via upload

---
+++ function(UDF) & logic Questions/Question-4/main.js
@@ -0,0 +1,45 @@
+function armstrong(n)
+{
+    let num = n ;
+    let count = 0;
+    let result =0 ;
+
+    while(num>0)
+    {
+        num = Math.floor(num / 10)
+        count++;
+    }
+
+    num = n;
+
+    for(let i=1; i<=count; i++)
+    {
+        let last_digit = num % 10;
+        let mult = 1;
+
+        for(let j=1; j<=count; j++)
+        {
+                mult = mult * last_digit;
+        }
+
+        result = result + mult ;
+        num = Math.floor(num  / 10 );
+    }
+
+    if(result === n)
+    {
+        document.write(n + " is a Armstrong number !")
+    }
+    else{
+        document.write(n + " is not a  Armstrong number !")
+    }
+
+
+
+}
+ 
+
+let  a = prompt("Enter a number : ");
+n = parseInt(a);
+
+armstrong(n)
