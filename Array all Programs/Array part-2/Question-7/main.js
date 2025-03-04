332c269ae041e656738cd55d9284d16d54fe1754
Bhupendra Patil <bhupendrakp2008@gmail.com>
2025-03-04 19:35:26 +0530

    Update main.js

--- Array all Programs/Array part-2/Question-7/main.js
+++ Array all Programs/Array part-2/Question-7/main.js
@@ -1,22 +1,23 @@
-let array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
-let indian_cricketers = array_is;
 
-document.write("Defined Array is :-  "+  indian_cricketers + " <hr> ")
+
+let array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
+
+document.write("Defined Array is :-  "+  array_is + " <hr><br> ")
 
 //.Push
 {
     let first = array_is.push(" Akshar Patel ");
-    document.write("Array using .Push :-  "  + array_is + "<hr>")
+    document.write(" 1)  &nbsp&nbsp " + "Array using .Push :-  "  + array_is + "<hr><br>")
 }
+
 
 
 //.pop 
 {
      array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
-    indian_cricketers = array_is;
 
     let second = array_is.pop()
-    document.write("Array using .Pop :-  "  + second + "<hr>")
+    document.write(" 2)  &nbsp&nbsp " +"Array using .Pop :-  "  + second + "<hr><br>")
 }
 
 
@@ -24,10 +25,9 @@ document.write("Defined Array is :-  "+  indian_cricketers + " <hr> ")
 //.shift
 {
     array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
-    indian_cricketers = array_is;
 
     let third = array_is.shift()
-    document.write("Array using .Shift :-  "  + third + "<hr>")
+    document.write(" 3)  &nbsp&nbsp " +"Array using .Shift :-  "  + third + "<hr><br>")
 }
 
 
@@ -35,20 +35,18 @@ document.write("Defined Array is :-  "+  indian_cricketers + " <hr> ")
 //.unshift
 {
     array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
-    indian_cricketers = array_is;
 
     let fourth = array_is.unshift("MS Dhoni")
-    document.write("Array using .Unshift :-  "  + indian_cricketers + "<hr>")
+    document.write(" 4)  &nbsp&nbsp " +"Array using .Unshift :-  "  + array_is + "<hr><br>")
 }
 
 
 //.Join
 {
     array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
-    indian_cricketers = array_is;
 
     let fifth = array_is.join(" *_* ")
-    document.write("Array using .Join :-  "  + fifth + "<hr>")
+    document.write(" 5)  &nbsp&nbsp " +"Array using .Join :-  "  + fifth + "<hr><br>")
 
 }
 
@@ -57,13 +55,12 @@ document.write("Defined Array is :-  "+  indian_cricketers + " <hr> ")
 {
     array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
     nums = [10 , 20 ,30 ,40, 50 ,60 ,70 ,80 ,90 ]
-    indian_cricketers = array_is;
 
     let sixth = array_is.sort()
-    document.write("Array using .Sort :-  "  + sixth + "<hr>")
+    document.write(" 6.1 )  &nbsp&nbsp " +"Array using .Sort :-  "  + sixth + "<br><br>")
     
     let sixth_ = nums.sort()
-    document.write("Array using .Sort :-  "  + sixth_ + "<hr>") 
+    document.write(" 6.2 )  &nbsp&nbsp " + "Array using .Sort :-  "  + sixth_ + "<hr><br>") 
 
 }
 
@@ -71,12 +68,59 @@ document.write("Defined Array is :-  "+  indian_cricketers + " <hr> ")
 //.at
 {
     array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
-    indian_cricketers = array_is;
 
     let seven = array_is.at(6)
-    document.write("Array using .at :-  "  + seven + "<hr>")
+    document.write(" 7)  &nbsp&nbsp " +"Array using .at :-  "  + seven + "<hr><br>")
 
 }
 
 
  
+//splice 
+{
+    array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
+
+    let eight = array_is.splice(2,1)
+    document.write(" 8.1 )  &nbsp&nbsp " + "Array using .splice  :-  "  + eight + "<br><br>")
+    
+    let eight_ = array_is.splice(2,1, " Varun Chakravarti")
+    document.write(" 8.2 )  &nbsp&nbsp " + "Array using .splice :-  "  + array_is + "<hr><br>")
+    
+
+}
+
+
+//slice 
+{
+    array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
+
+    let nine = array_is.slice(2,5)
+    document.write(" 9)  &nbsp&nbsp " + "Array using .slice :-  "  + nine + "<hr><br>")
+
+}
+
+
+//indexoff
+{
+
+    array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
+
+    let tenth = array_is.indexOf(" Shikhar Dhawan ")
+    document.write(" 10)  &nbsp&nbsp " + "Array using .Indexof :-  "  + tenth + "<hr><br>")
+    
+}
+
+
+
+
+//concat
+{
+
+    array_is  = ["Virat Kohli ", " Rohit Sharma ", " Shikhar Dhawan ", " KL Rahul ", " Jasprit Bumrah ", " Rishabh Pant ", " Hardik Pandya "]
+    let array_two = [' MS Dhoni ', ' Sachin Tendulkar '];
+
+    let eleventh = array_two.concat(array_is)
+    document.write(" 11)  &nbsp&nbsp " + "Array using .Concat :-  "  + eleventh + "<hr><br>")
+    
+}
+
